e00ad9a24ca8a67fd78354bc4b88683cc8121865
Benilson0189 <benilsondossantoscativa@gmail.com>
2026-01-30 00:47:16 +0100

    Update main.tsx
    Actualização

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom"; // <- esta linha nova
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>         {/* <- aqui envolve o App */}
+      <App />
+    </BrowserRouter>        {/* <- fecha o BrowserRouter */}
   </React.StrictMode>
 );
